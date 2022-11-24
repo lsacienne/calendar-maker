56f17320445194acf4c8aec86729e600e9f56ad3
lsacienne <alexandre.viala@outlook.com>
2022-11-24 22:06:54 +0100

    :sparkles: Add of widget to display a date

---
+++ src/components/from_components/DateField.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="date-item">
+        <label for="date_debut">{{labeltext}}</label>
+        <input type="date" name="date_debut" id="date_debut_cours">
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'DateField',
+  data () {
+    return {
+      test: String
+    }
+  },
+  props: {
+    labeltext: String
+  }
+})
+</script>
+
+<style scoped>
+  .date-item {
+    display: inline-flex;
+    width: 60%;
+    justify-content: space-between;
+  }
+
+  .date-item label {
+    font-size: 1.1rem;
+    font-weight: bold;
+  }
+
+  .date-item label::before {
+    content: "📅";
+    margin-right: 0.5rem;
+  }
+</style>
