5ba9a6bdd024a5804b5f481dbf91dd776a4fed25
lsacienne <alexandre.viala@outlook.com>
2023-09-12 14:05:22 -0400

    ♻️ Correct user data

--- src/components/UserData.vue
+++ src/components/UserData.vue
@@ -2,7 +2,8 @@
   <article>
     <h1>1. Coller votre emploi du temps</h1>
     <form action="">
-      <textarea name="schedule-handler" ref="scheduleArea" id="schedule-handler" placeholder="Coller votre emploi du temps ici"></textarea>
+      <textarea name="schedule-handler" ref="scheduleArea" id="schedule-handler"
+        placeholder="Coller votre emploi du temps ici"></textarea>
       <div class="date-grid">
         <DateContainer title="Début des cours :">
           <DateField labeltext="" ref="startCourses" v-on:dateChange="handleCoursesStart()"></DateField>
@@ -33,6 +34,7 @@ import DateContainer from './form_components/DateContainer.vue'
 import DateField from './form_components/DateField.vue'
 import SubmitButton from './form_components/SubmitButton.vue'
 import { createToaster } from '@meforma/vue-toaster'
+import { ShceduleItem as ScheduleItem, frenchDays } from '@/models/types'
 
 const toaster = createToaster()
 
@@ -40,7 +42,7 @@ export default defineComponent({
   name: 'UserData',
   components: { DateField, DateContainer, SubmitButton },
   methods: {
-    handleCoursesStart () {
+    handleCoursesStart() {
       const startCourses = this.$refs.startCourses as typeof DateField
       const endCourses = this.$refs.endCourses as typeof DateField
       const startRest1 = this.$refs.startRest1 as typeof DateField
@@ -63,7 +65,7 @@ export default defineComponent({
       startRest1.enable()
       endRest1.enable()
     },
-    handleStart1 () {
+    handleStart1() {
       const startRest1 = this.$refs.startRest1 as typeof DateField
       const endRest1 = this.$refs.endRest1 as typeof DateField
       let endRestMinDate = new Date(startRest1.getValue()) as Date
@@ -72,7 +74,7 @@ export default defineComponent({
       endRest1.setValue(endRestMinDate)
       this.enableDate2()
     },
-    handleEnd1 () {
+    handleEnd1() {
       const startRest1 = this.$refs.startRest1 as typeof DateField
       const endRest1 = this.$refs.endRest1 as typeof DateField
       let startNewDate = new Date(endRest1.getValue()) as Date
@@ -80,7 +82,7 @@ export default defineComponent({
       startRest1.setValue(startNewDate)
       this.enableDate2()
     },
-    handleStart2 () {
+    handleStart2() {
       const startRest2 = this.$refs.startRest1 as typeof DateField
       const endRest2 = this.$refs.endRest1 as typeof DateField
       let endRestMinDate = new Date(startRest2.getValue()) as Date
@@ -88,7 +90,7 @@ export default defineComponent({
       endRest2.setMin(endRestMinDate)
       endRest2.setValue(endRestMinDate)
     },
-    enableDate2 () {
+    enableDate2() {
       const endRest1 = this.$refs.endRest1 as typeof DateField
       const startRest2 = this.$refs.startRest2 as typeof DateField
       const endRest2 = this.$refs.endRest2 as typeof DateField
@@ -104,16 +106,16 @@ export default defineComponent({
       startRest2.enable()
       endRest2.enable()
     },
-    addDay (date: Date, days: number): Date {
+    addDay(date: Date, days: number): Date {
       const resultDate = new Date(date)
       resultDate.setDate(resultDate.getDate() + days)
       return resultDate
     },
-    sendData (): {
-      courses: {start: string, end: string},
+    sendData(): {
+      courses: { start: string, end: string },
       schedule: Array<object>,
-      rests: Array<{start: string, end: string}>
-      } | null {
+      rests: Array<{ start: string, end: string }>
+    } | null {
       const scheduleArea = this.$refs.scheduleArea as HTMLTextAreaElement
       const startCourses = this.$refs.startCourses as typeof DateField
       const endCourses = this.$refs.endCourses as typeof DateField
@@ -137,12 +139,54 @@ export default defineComponent({
 
       const scheduleStr = scheduleArea.value
       const schedule = scheduleStr.split('\n').map(line => line.split(/\t+/)) as Array<Array<string>>
-      const scheduleTable = []
+      const scheduleTable = [] as Array<ScheduleItem>
 
       for (const line of schedule) {
-        let mode = '' as string
-        let classroom = '' as string
+        // let mode = '' as string
+        // let classroom = '' as string
+        // let hasGroup = false
+        let startHourSet = false
+        const resultObject: ScheduleItem = {
+          uv: '',
+          type: '',
+          day: '',
+          startHour: '',
+          endHour: '',
+          frequency: '',
+          classroom: '',
+          mode: '',
+          group: ''
+        }
+        line.forEach((value, i) => {
+          if (i === 0) {
+            resultObject.uv = value
+          } else if (/(TD|TP|CM)[0-9]/.test(value)) {
+            resultObject.type = value
+          } else if (frenchDays.includes(value)) {
+            resultObject.day = value
+          } else if (/[A-Z]/g.test(value) && value.length === 1) {
+            resultObject.group = value
+          } else if (/[0-9]{2}:[0-9]{2}/.test(value)) {
+            if (!startHourSet) {
+              resultObject.startHour = value
+            } else {
+              resultObject.endHour = value
+            }
+            startHourSet = true
+          } else if (/[1-2]/.test(value) && value.length === 1) {
+            resultObject.frequency = value
+          } else if (value.match(/(Présentiel|Distanciel)/g)) {
+            resultObject.mode = value
+          } else {
+            resultObject.classroom = value
+          }
+        })/*
+        if (!frenchDays.includes(line[2])) {
+          resultObject.group = line[2]
+          hasGroup = true
+        }
         if (line.length > 7) {
+
           classroom = line[7]
           mode = line[6]
         } else if (line[6] === 'Distanciel') {
@@ -150,17 +194,9 @@ export default defineComponent({
         } else {
           classroom = line[6]
         }
+        */
 
-        scheduleTable.push({
-          uv: line[0],
-          type: line[1],
-          day: line[2],
-          startHour: line[3],
-          endHour: line[4],
-          frequency: line[5],
-          classroom: classroom,
-          mode: mode
-        })
+        scheduleTable.push(resultObject)
       }
 
       if (scheduleStr === '') {
@@ -210,6 +246,7 @@ export default defineComponent({
         courses: courses,
         rests: restArray
       })
+      console.log(scheduleTable)
       return {
         schedule: scheduleTable,
         courses: courses,
@@ -221,46 +258,46 @@ export default defineComponent({
 </script>
 
 <style scoped>
-    article {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      background-color: rgba(255, 216, 59, 0.5);
-      padding: 1.5rem;
-      box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-      border-radius: 1rem;
-    }
+article {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: rgba(255, 216, 59, 0.5);
+  padding: 1.5rem;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  border-radius: 1rem;
+}
 
-    h1 {
-      margin-left: 4rem;
-      align-self: flex-start;
-    }
+h1 {
+  margin-left: 4rem;
+  align-self: flex-start;
+}
 
-    article form {
-      display: flex;
-      width: 80%;
-      flex-direction: column;
-      align-items: flex-start;
-      gap: 1rem;
-    }
+article form {
+  display: flex;
+  width: 80%;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 1rem;
+}
 
-    form textarea {
-      border-radius: 1rem;
-      width: 91%;
-      height: 30vh;
-      padding: 4%;
-      resize: none;
-      background-color: rgb(255, 255, 255);
-      border: solid rgb(121, 121, 209) 0.2rem;
-      /*border: solid rgb(252, 204, 12) 0.2rem;*/
-    }
+form textarea {
+  border-radius: 1rem;
+  width: 91%;
+  height: 30vh;
+  padding: 4%;
+  resize: none;
+  background-color: rgb(255, 255, 255);
+  border: solid rgb(121, 121, 209) 0.2rem;
+  /*border: solid rgb(252, 204, 12) 0.2rem;*/
+}
 
-    .date-grid {
-      display: grid;
-      grid-template-columns: 1fr 1fr;
-      width: 100%;
-      gap: 0.5rem;
-      border-radius: 1rem;
-      background-color: rgb(121, 121, 209,0.6);
-    }
+.date-grid {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  width: 100%;
+  gap: 0.5rem;
+  border-radius: 1rem;
+  background-color: rgb(121, 121, 209, 0.6);
+}
 </style>
