fd307adecaf5c38a7d08fe292e9ab9e804fca2b2
lsacienne <alexandre.viala@outlook.com>
2024-10-20 15:50:32 +0200

    ♻️ Reformat types spaces

--- src/models/types.ts
+++ src/models/types.ts
@@ -1,72 +1,76 @@
 export type DateChooser = {
-    id: number,
-    uv: string,
-    type: string,
-    day: string,
-    date1: Date | undefined,
-    date2: Date | undefined,
-    chosenDate: Date | number | null
+  id: number,
+  uv: string,
+  type: string,
+  day: string,
+  date1: Date | undefined,
+  date2: Date | undefined,
+  chosenDate: Date | number | null
 }
 
 export type ScheduleWithChoice = {
-    uv: string,
-    type: string,
-    day: string,
-    startHour: string,
-    endHour: string,
-    frequency: string,
-    classroom: string,
-    mode: string,
-    group: string,
-    chosenDate: Date | number | null
+  uv: string,
+  type: string,
+  day: string,
+  startHour: string,
+  endHour: string,
+  frequency: string,
+  classroom: string,
+  mode: string,
+  group: string,
+  chosenDate: Date | number | null
 }
 
 export type ScheduleItem = {
-    uv: string,
-    type: string,
-    day: string,
-    group: string,
-    startHour: string,
-    endHour: string,
-    frequency: string,
-    classroom: string,
-    mode: string
+  uv: string,
+  type: string,
+  day: string,
+  group: string,
+  startHour: string,
+  endHour: string,
+  frequency: string,
+  classroom: string,
+  mode: string
 }
 
 export type Schedule = {
-    courses: {
-        start: string,
-        end: string
-    },
-    rests: Array<{
-        start: string,
-        end: string
-    }>,
-    schedule: Array<ScheduleWithChoice>
+  courses: {
+    start: string,
+    end: string
+  },
+  rests: Array<{
+    start: string,
+    end: string
+  }>,
+  schedule: Array<ScheduleWithChoice>
 }
 
 export type DateableEvent = {
-    uv: string,
-    type: string,
-    group: string,
-    firstDate: string,
-    lastDate: string,
-    duration: number,
-    frequency: number,
-    classroom: string,
-    mode: string
+  uv: string,
+  type: string,
+  group: string,
+  firstDate: string,
+  lastDate: string,
+  duration: number,
+  frequency: number,
+  classroom: string,
+  mode: string
 }
 
 export type icsEvent = {
-    start: [number, number, number, number, number],
-    title: string,
-    duration: {
-        hours: number,
-        minutes: number
-    },
-    description: string,
-    location: string,
-    recurrenceRule: string
+  start: [number, number, number, number, number],
+  title: string,
+  duration: {
+    hours: number,
+    minutes: number
+  },
+  description: string,
+  location: string,
+  recurrenceRule: string
+}
+
+export type IcsContainer = {
+  events: Array<icsEvent>
 }
 
 export const frenchDays = ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche']
