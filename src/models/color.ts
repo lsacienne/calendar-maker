3b41c14b6d1ff86a8d939e6dca5a6c5d99816561
lsacienne <alexandre.viala@outlook.com>
2024-10-20 20:44:29 +0200

    🏷️ Create color type

---
+++ src/models/color.ts
@@ -0,0 +1,122 @@
+export class Color {
+    private _r: number;
+    private _g: number;
+    private _b: number;
+
+    public get r(): number {
+        return this._r;
+    }
+
+    public get g(): number {
+        return this._g;
+    }
+
+    public get b(): number {
+        return this._b;
+    }
+
+    private checkCorrectColorValue(value: number): number {
+        if (value < 0) {
+            return 0;
+        }
+        if (value > 255) {
+            return 255;
+        }
+        if (!Number.isInteger(value)) {
+            return Math.round(value);
+        }
+        return value;
+    }
+
+    public set r(value: number) {
+        this._r = this.checkCorrectColorValue(value);
+    }
+
+    public set g(value: number) {
+        this._g = this.checkCorrectColorValue(value);
+    }
+
+    public set b(value: number) {
+        this._b = this.checkCorrectColorValue(value);
+    }
+
+    private constructor() {
+        this._r = 0;
+        this._g = 0;
+        this._b = 0;
+    }
+    
+    static fromRGB(r: number, g: number, b: number): Color {
+        const color = new Color();
+        color.r = r;
+        color.g = g;
+        color.b = b;
+        return color;
+    }
+
+    static fromHex(hex: string): Color {
+        const color = new Color();
+        const colorArray: Array<number> = hex.match(/[0-9a-fA-F]{2}/g)?.map((value) => parseInt(value, 16)) || [];
+        if (colorArray.length >= 3) {
+            color.r = colorArray[0];
+            color.g = colorArray[1];
+            color.b = colorArray[2];
+        } else {
+            throw new Error('Invalid hex color!');
+        }
+        return color;
+    }
+
+    toHexString(): string {
+        const redHex = this._r.toString(16).padStart(2, '0');
+        const greenHex = this._g.toString(16).padStart(2, '0');
+        const blueHex = this._b.toString(16).padStart(2, '0');
+
+        return `#${redHex}${greenHex}${blueHex}`;
+    }
+
+    darkenColor(t: number): Color {
+        return Color.lerpColor(this, Color.black, t);
+    }
+
+    lightenColor(t: number): Color {
+        return Color.lerpColor(this, Color.white, t);
+    }
+
+    static lerpColor(color1: Color, color2: Color, t: number): Color {
+        t = Math.max(0, Math.min(1, t));
+        return Color.fromRGB(
+            color1.r + (color2.r - color1.r) * t,
+            color1.g + (color2.g - color1.g) * t,
+            color1.b + (color2.b - color1.b) * t
+        );
+    }
+
+    static get randomColor(): Color {
+        return Color.fromRGB(
+            Math.floor(Math.random() * 256),
+            Math.floor(Math.random() * 256),
+            Math.floor(Math.random() * 256)
+        );
+    }
+
+    static get black(): Color {
+        return Color.fromRGB(0, 0, 0);
+    }
+
+    static get white(): Color {
+        return Color.fromRGB(255, 255, 255);
+    }
+
+    static get red(): Color {
+        return Color.fromRGB(255, 0, 0);
+    }
+
+    static get green(): Color {
+        return Color.fromRGB(0, 255, 0);
+    }
+
+    static get blue(): Color {
+        return Color.fromRGB(0, 0, 255);
+    }
+}
